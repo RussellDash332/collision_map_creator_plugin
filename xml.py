0a1507480a407aea25764c51e1da074ffb5658a3
RussellDash332 <russellsaerang@gmail.com>
2023-05-28 06:55:33 +0800

    Modify XML

--- xml.py
+++ xml.py
@@ -1,16 +1,16 @@
 import lxml.etree as et
 
-tree = et.fromstring(open('i2r_lvl9.world').read())
+tree = et.fromstring(open('lvl11_final.world').read())
 
 print('Before:', len(tree.findall('.//model')))
 
 for model in tree.findall('.//model'):
-    for check in ['chair', 'table', 'bin', 'drawer', 'cubicle', 'sofa', 'toilet', 'cylinder', 'sink', 'kicthen', 'shelf', 'mop', 'openrobotics']:
+    for check in ['chair', 'table', 'bin', 'drawer', 'cubicle', 'sofa', 'toilet', 'cylinder', 'sink', 'kicthen', 'shelf', 'mop', 'openrobotics', 'door', 'clone']:
         if check in model.get('name').lower() or len(model.get('name').lower()) < 5:
             model.getparent().remove(model)
             break
 
 print('After:', len(tree.findall('.//model')))
 
-with open('lvl9.world', 'w+') as f:
+with open('lvl11.world', 'w+') as f:
     f.write(et.tostring(tree, pretty_print=True).decode())
